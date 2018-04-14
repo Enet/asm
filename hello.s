29f039ff2e8eada85e3a7b73bae14cbd7c56316a
Stepan Zhevak <enet@protonmail.ch>
2018-04-14 17:09:44 +0300

    Refactoring

--- hello.s
+++ hello.s
@@ -1,22 +1,23 @@
 .section .rodata
+    .set exit_system_call, 1
     .set write_system_call, 4
     .set std_out, 1
     .set interruption_code, 0x80
-    .set exit_code_success, 0
 
     hello_world:
         .string "Hello world!\n"
         .set hello_world_length, . - hello_world - 1
 
 .text
-    .globl main
+    .globl _start
 
-main:
+_start:
     movl $write_system_call, %eax
     movl $std_out, %ebx
     movl $hello_world, %ecx
     movl $hello_world_length, %edx
     int $interruption_code
 
-    movl $exit_code_success, %eax
-    ret
+    movl $exit_system_call, %eax
+    xor %ebx, %ebx
+    int $interruption_code
