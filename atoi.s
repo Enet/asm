ea6b7cf1172a3ff3699461a1446313d36c263675
Stepan Zhevak <enet@protonmail.ch>
2018-04-21 14:03:17 +0300

    Refactoring of atoi function

--- atoi.s
+++ atoi.s
@@ -1,20 +1,8 @@
-// unoptimized atoi function, that convert string (first argument) to integer
+/* atoi function, that converts string to integer */
 
 .section .rodata
     printf_output:
         .string "%d = %d * 2\n"
-    number_0:
-        .long '0'
-    number_9:
-        .long '9'
-    plus:
-        .long '+'
-    minus:
-        .long '-'
-    space:
-        .long ' '
-    new_line:
-        .long '\n'
 
 .text
     .globl main
@@ -23,113 +11,110 @@ custom_atoi:
     pushl %ebp
     movl %esp, %ebp
     movl 8(%ebp), %eax
+/* ignore spaces at the beginning */
 atoi_space_loop_body:
-    movl (%eax), %ebx
-    andl $0x000000ff, %ebx
-    cmpl space, %ebx
+    movb (%eax), %bl
+    cmpb $' ', %bl
     je atoi_space_loop_inc
-    cmpl $0, %ebx
+    cmpb $0, %bl
     je atoi_error
     jmp atoi_sign
 atoi_space_loop_inc:
     incl %eax
     jmp atoi_space_loop_body
+/* determine sign and store to %dl (0 means +, 1 means -) */
 atoi_sign:
-    movl $0, %esi
-    cmpl plus, %ebx
+    movb $0, %dl
+    cmpb $'+', %bl
     je atoi_plus
-    cmpl minus, %ebx
+    cmpb $'-', %bl
     je atoi_minus
     jmp atoi_digit
 atoi_minus:
-    movl $1, %esi
+    incb %dl
 atoi_plus:
     incl %eax
+/* push all the digits to the stack (%ecx is the number of digits) */
 atoi_digit:
-    pushl %esi
-    movl $0, %ecx
+    xorl %ecx, %ecx
 atoi_digit_loop_body:
-    movl (%eax), %ebx
-    andl $0x000000ff, %ebx
-    cmpl number_0, %ebx
+    movb (%eax), %bl
+    cmpb $'0', %bl
     jl atoi_digit_loop_break
-    cmpl number_9, %ebx
+    cmpb $'9', %bl
     jg atoi_digit_loop_break
-    subl number_0, %ebx
+    subb $'0', %bl
     pushl %ebx
     incl %ecx
 atoi_digit_loop_inc:
     incl %eax
     jmp atoi_digit_loop_body
 atoi_digit_loop_break:
-    cmpl $0, %ebx
+    cmpb $0, %bl
     je atoi_sum_loop_init
-    cmpl space, %ebx
+    cmpb $' ', %bl
     je atoi_sum_loop_init
-    cmpl new_line, %ebx
+    cmpb $'\n', %bl
     je atoi_sum_loop_init
     jmp atoi_error
+/* pop from the stack and sum the digits */
 atoi_sum_loop_init:
-    movl $0, %esi
-    movl $0, %edi
+    xorl %eax, %eax
+    xorl %edi, %edi
 atoi_sum_loop_body:
-    popl %eax
-    pushl %ecx
-    movl %edi, %ecx
-atoi_multi_loop_inc:
-    cmpl $0, %ecx
-    je atoi_multi_loop_break
-atoi_multi_loop_body:
-    leal (%eax,%eax,4), %eax
-    shl $1, %eax
-    loop atoi_multi_loop_body
-    jmp atoi_multi_loop_break
-atoi_multi_loop_break:
-    popl %ecx
-    addl %eax, %esi
-    incl %edi
-    loop atoi_sum_loop_body
-    movl %esi, %eax
     popl %esi
-    xor %ebx, %ebx
-    testl %esi, %esi
+    pushl %edi
+atoi_multi_loop_inc:
+    cmpl $0, %edi
+    je atoi_sum_loop_inc
+atoi_multi_loop_body:
+    leal (%esi,%esi,4), %esi
+    shl $1, %esi
+    decl %edi
+    jmp atoi_multi_loop_inc
+atoi_sum_loop_inc:
+    popl %edi
+    incl %edi
+    addl %esi, %eax
+    loop atoi_sum_loop_body
+/* apply the sign from %dl and return the value */
+    xorl %ebx, %ebx
+    testb %dl, %dl
     jz atoi_return
     notl %eax
     incl %eax
     jmp atoi_return
-atoi_sum_loop_inc:
-    decl %ecx
+/* %eax contains result, %ebx contains error code */
 atoi_error:
     movl $-1, %ebx
-    jmp atoi_return
 atoi_return:
     movl %ebp, %esp
     popl %ebp
     ret
 
 main:
+no_arguments:
     movl 4(%esp), %eax
     cmpl $2, %eax
     jl error
-
+custom_atoi_call:
     movl 8(%esp), %eax
     pushl 4(%eax)
     call custom_atoi
     addl $4, %esp
-
+custom_atoi_error:
     testl %ebx, %ebx
     jnz error
-
+custom_atoi_output:
     pushl %eax
     shl $1, %eax
     pushl %eax
     pushl $printf_output
     call printf
     addl $12, %esp
-
-    xor %eax, %eax
+end:
+    xorl %eax, %eax
     ret
-
 error:
     movl $1, %eax
     ret
